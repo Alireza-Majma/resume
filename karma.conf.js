f7482feea8aa8737b687aa111b4a049b659ad3e7
Alireza Majma <alireza_majma@yahoo.co>
2019-11-28 18:40:12 -0500

    rename project to host-resume

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/alireza-web'),
+      dir: require('path').join(__dirname, './coverage/host-resume'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
