afe3037cef91af01360a5a5d46572cf2f91029b3
Alireza Majma <alireza_majma@yahoo.co>
2019-11-28 23:39:26 -0500

    develope home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponentComponent } from '../home-component/home-component.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home' , component: HomeComponentComponent},
+  {path: '' ,  redirectTo: '/home',  pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
