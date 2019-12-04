3b71b3c9b3279f4a8292da229733bbcdde26f63a
Alireza Majma <alireza_majma@yahoo.co>
2019-12-03 23:20:32 -0500

    after google verification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,6 @@ import { HomeComponent } from './components/home-component/home.component';
 
 
 const routes: Routes = [
-  // {path: 'home' , component: HomeComponent},
-  // {path: 'education-info' , component: EducationInfoComponent},
-  // {path: 'technical-info' , component: TechnicalInfoComponent},
-  // {path: 'career-highlight' , component: CareerHighlightComponent},
-  // {path: 'experience-info' , component: ExperienceInfoComponent},
   {path: 'info', loadChildren: () => import('./info-module/info.module').then(m => m.InfoModule) },
   {path: '' ,  component: HomeComponent,  pathMatch: 'full'},
   {path: '**' ,  component: HomeComponent}
