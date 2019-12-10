2cf3d3263af07aa524e95153799e4ad4ba8ed0d7
Alireza Majma <alireza_majma@yahoo.co>
2019-12-10 14:01:22 -0500

    update key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   jexia: {
     projectID: 'e4158913-ef39-4731-ade9-b8da5d9ae283',
     projectURL: 'https://e4158913-ef39-4731-ade9-b8da5d9ae283.app.jexia.com',
-    key: '39bb377f-285e-4356-9836-db01a7e1642c',
-    secret: 'QsfPG5BpqVIYw1HTzGm+1z8iuR7fR+JlgN1dS6GAyHBOFvHmq1u71KNyPxCT2KlTkLlpjU6sghgojRrzggN1dw==',
+    key: 'c805324e-1844-4483-8267-d403a4e46981',
+    secret: 'oadIIOibYs4OhR0PVDzEp38k99EdomTVi9FVcyCkQHW2/gUYA9atg9nhgSycbCdCTEHWq4J73eovCcO4CS83Ig=='
   }
 };
