6f5fe966f3ed483fc473a1a9f93e976cd0db1aae
Alireza Majma <alireza_majma@yahoo.co>
2019-12-11 23:10:12 -0500

    after jexia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { environment } from 'src/environments/environment';
       HomeComponent
    ],
    imports: [
-      BrowserModule,
+      BrowserModule.withServerTransition({ appId: 'serverApp' }),
       AppRoutingModule,
       HttpClientModule,
       RouterModule
